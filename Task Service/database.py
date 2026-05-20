5171da98100e94fd9bca0e43644b4eb4323f59d3
Rupam Gope <9681rupam@gmail.com>
2026-05-20 23:57:51 +0530

    Update and rename database.py.txt to database.py

---
+++ Task Service/database.py
@@ -0,0 +1,12 @@
+tasks = [
+    {
+        "id": 1,
+        "title": "Distributed Systems Task",
+        "status": "In Progress"
+    },
+    {
+        "id": 2,
+        "title": "Microservices Project",
+        "status": "Completed"
+    }
+]
